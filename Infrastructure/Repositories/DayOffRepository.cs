8d5290e239e59798294659840a95f96238fd321d
4hmetuyar <4hmetuyar@gmail.com>
2015-12-03 10:19:25 +0200

    DayOff Repositorye yeni kayıt ekleme metodu eklendi.

--- Infrastructure/Repositories/DayOffRepository.cs
+++ Infrastructure/Repositories/DayOffRepository.cs
@@ -6,13 +6,15 @@ using System.Threading.Tasks;
 using Huwax.Dal;
 using Infrastructure.Infrastructure;
 using Infrastructure.Interfaces;
+using Infrastructure.Models;
 
 namespace Infrastructure.Repositories
 {
- 
+
 
     public interface IDayOffRepository : IGenericRepository<DayOff>
     {
+        DayOff AddNewDayOffByDayOffModel(DayOffModel model);
     }
 
     public class DayOffRepository : GenericRepository<DayOff>, IDayOffRepository
@@ -21,6 +23,33 @@ namespace Infrastructure.Repositories
             : base(databaseFactory)
         {
 
+        }
+
+        public DayOff AddNewDayOffByDayOffModel(DayOffModel model)
+        {
+            try
+            {
+                var add = new DayOff
+                {
+                    IsDeleted = false,
+                    CreatedDate = model.CreatedDate,
+                    Date = model.Date,
+                    CratedById = model.CratedById,
+                    EndDate = model.EndDate,
+                    PersonnelId = model.PersonnelId,
+                    StartDate = model.StartDate,
+                    UserId = model.UserId
+
+                };
+                Add(add);
+                Commit();
+                return add;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
         }
     }
 }
