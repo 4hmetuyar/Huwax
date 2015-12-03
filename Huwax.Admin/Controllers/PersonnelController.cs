b28da18ba8e41760ca5868cb9b0ca7bc6d999765
4hmetuyar <4hmetuyar@gmail.com>
2015-12-03 11:16:52 +0200

    Personel ekleme sayfaları eklendi.

---
+++ Huwax.Admin/Controllers/PersonnelController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Huwax.Admin.Controllers
+{
+    public class PersonnelController : Controller
+    {
+        // GET: Personnel
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult AddPersonnel()
+        {
+            return View();
+        }
+    }
+}
