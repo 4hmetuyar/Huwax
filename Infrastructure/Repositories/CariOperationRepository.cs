acd6849946b77b09cebedc3f38ec94539941aaa5
4hmetuyar <4hmetuyar@gmail.com>
2015-12-03 09:51:50 +0200

    Cari Operation Repository düzenlendi.

--- Infrastructure/Repositories/CariOperationRepository.cs
+++ Infrastructure/Repositories/CariOperationRepository.cs
@@ -3,10 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Huwax.Dal;
+using Infrastructure.Infrastructure;
+using Infrastructure.Interfaces;
 
 namespace Infrastructure.Repositories
 {
-    class CariOperationRepository
+
+
+    public interface ICariOperationRepository : IGenericRepository<CariOperation>
     {
+    }
+
+    public class CariOperationRepository : GenericRepository<CariOperation>, ICariOperationRepository
+    {
+        public CariOperationRepository(IDatabaseFactory databaseFactory)
+            : base(databaseFactory)
+        {
+
+        }
     }
 }
