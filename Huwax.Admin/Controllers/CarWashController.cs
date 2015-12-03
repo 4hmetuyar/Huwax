17af474632bae2f60aa673fbf2c37477d05becff
4hmetuyar <4hmetuyar@gmail.com>
2015-12-03 11:20:07 +0200

    Gerekli Controllerlar eklendi.

---
+++ Huwax.Admin/Controllers/CarWashController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Huwax.Admin.Controllers
+{
+    public class CarWashController : Controller
+    {
+        // GET: CarWash
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
