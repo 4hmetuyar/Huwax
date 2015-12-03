2c97ed6d071938534ccb432ba3043521d85af4c8
4hmetuyar <4hmetuyar@gmail.com>
2015-12-03 10:09:42 +0200

    Cari Operation Add method added.

--- Infrastructure/Repositories/CariOperationRepository.cs
+++ Infrastructure/Repositories/CariOperationRepository.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Huwax.Dal;
 using Infrastructure.Infrastructure;
 using Infrastructure.Interfaces;
+using Infrastructure.Models;
 
 namespace Infrastructure.Repositories
 {
@@ -13,14 +14,40 @@ namespace Infrastructure.Repositories
 
     public interface ICariOperationRepository : IGenericRepository<CariOperation>
     {
-
-     }
+        CariOperation AddNewCariOperationByCariOperationModel(CariOperationModel model);
+    }
 
     public class CariOperationRepository : GenericRepository<CariOperation>, ICariOperationRepository
     {
         public CariOperationRepository(IDatabaseFactory databaseFactory)
             : base(databaseFactory)
         {
+
+        }
+
+        public CariOperation AddNewCariOperationByCariOperationModel(CariOperationModel model)
+        {
+            try
+            {
+                var add = new CariOperation
+                {
+                    CariId = model.CariId,
+                    CreatedById = model.CreatedById,
+                    CreatedDate = model.CreatedDate,
+                    Date = model.Date,
+                    Description = model.Description,
+                    IsDeleted = false,
+                    Status = model.Status,
+                    Total = model.Total
+
+                };
+                return add;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
 
         }
     }
