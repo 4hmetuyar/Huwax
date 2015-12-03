d3b8d374d821ffe28b057fef4a80b600416ce97b
4hmetuyar <4hmetuyar@gmail.com>
2015-12-03 10:03:17 +0200

    Proje Olarak Common eklendi.

---
+++ Huwax.Common/Enums.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Huwax.Common
+{
+    public static class Enums
+    {
+        public enum UserRole
+        {
+            Admin = 1,
+            Otomasyon = 2,
+            Finans = 3,
+            Satıs = 4,
+            Lojistik = 5,
+            Arac = 6,
+            Sofor = 7,
+            Yonetim = 8,
+            SahaMuduru = 9,
+            Bayi = 10
+        }
+
+
+    }
+}
